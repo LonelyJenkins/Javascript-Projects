40e67ff988cb67fca711350de6a0b999cc452c06
LonelyJenkins <124760225+LonelyJenkins@users.noreply.github.com>
2023-03-21 02:26:22 -0500

    New jss file including type operator

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,19 @@
+//printing booleans utilizing several different operators
+document.write("3" + 1415);
+document.write(typeof "string");
+document.write("string" === "string");
+document.write(10==10);
+document.write(15!=10);
+document.write(10>11 || 50<60);
+document.write(420>360 && 500>250);
+
+
+function myFunction()//utilizing isNan
+{
+    document.getElementById("test").innerHTML=isNaN("String");//This will display as 'true'
+    //document.getElementById("test").innerHTML=isNaN("420");//This will display as 'false'
+
+}
+
+console.log(10-3);
+console.log(50<20);
